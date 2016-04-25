59a2ebb9e3ffd23673be555545609229a16ef214
DePascual <carolinadepascualjimenez@gmail.com>
2016-04-25 18:17:40 +0200

    No funciona

---
+++ MenuLayout/MenuLayout/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MenuLayout.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(
+                new ScriptBundle("~/bundles/libraries")
+                    .Include("~/Scripts/angular/angular.js")
+                    .Include("~/Scripts/angular/angular-route.js")
+                    .Include("~/Scripts/angular/angular-resource.js")
+                    .Include("~/Scripts/bootstrap.js")
+                    .Include("~/Scripts/jquery-1.10.2.js")
+                );
+
+            //including all the css used in our app
+            bundles.Add(
+                new StyleBundle("~/bundles/css")
+                    .Include("~/Content/bootstrap/bootstrap.css")
+                    .Include("~/Content/bootstrap/bootstrap-theme.css")
+                    .Include("~/Content/MenuLayout.css")
+                );
+
+            //including the javascript files in our app
+            bundles.Add(
+                new ScriptBundle("~/bundles/MenuLayout")
+                    .Include("~/Scripts/MenuLayout/MenuLayout.js")
+                    .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                );
+        }
+    }
+}
---
+++ MenuLayout/MenuLayout/Scripts/MenuLayout/MenuLayout.js
@@ -0,0 +1,19 @@
+﻿var MenuLayout = angular.module('MenuLayout', ['ngRoute']);
+
+MenuLayout.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/One.cshtml'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/Two.cshtml'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/Three.cshtml'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+MenuLayout.config(configFunction);
