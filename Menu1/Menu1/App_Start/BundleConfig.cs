ed68332c33c60708d84b918ef4705f4138f85982
DePascual <carolinadepascualjimenez@gmail.com>
2016-04-25 14:35:53 +0200

    Primer menu dinamico cutre

---
+++ Menu1/Menu1/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Menu1.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+
+            bundles.Add(new ScriptBundle("~/bundles/Menu1")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .Include("~/Scripts/Menu1.js"));
+
+     
+        }
+    }
+}
---
+++ Menu1/Menu1/Scripts/Menu1.js
@@ -0,0 +1,19 @@
+﻿var Menu1 = angular.module('Menu1', ['ngRoute']);
+
+Menu1.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+Menu1.config(configFunction);
