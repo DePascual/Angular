1fe3751620c2f5057f5add09429218ced716b7a9
DePascual <carolinadepascualjimenez@gmail.com>
2016-05-03 21:41:43 +0200

    Probando ng-repeat Falta meter estilos

---
+++ Angular1/Angular1/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Angular1.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Librerias descargadas
+            bundles.Add(
+                new ScriptBundle("~/bundles/libraries")
+                    .Include("~/Scripts/Angular/angular.js")
+                    .Include("~/Scripts/Angular/angular-route.js")
+                    .Include("~/Scripts/Angular/angular-resource.js")
+                    .Include("~/Scripts/bootstrap.js")
+                    .Include("~/Scripts/jquery-1.9.1.js")
+                );
+
+            //Archivos CSS de la app
+            bundles.Add(
+                new StyleBundle("~/bundles/css")
+                    .Include("~/Content/bootstrap.css")
+                    .Include("~/Content/bootstrap-theme.css")
+                );
+
+            //Archivos javascript de la app
+            bundles.Add(
+                new ScriptBundle("~/bundles/misScripts")
+                .Include("~/Scripts/misScripts.js")
+               );
+
+        }
+    }
+}
---
+++ Angular1/Angular1/Scripts/misScripts.js
@@ -0,0 +1,9 @@
+﻿var cuaderno = angular.module('cuaderno', []);
+cuaderno.controller('alumnosController', function ($scope) {
+    $scope.alumnos = [
+           { nombre: "Carolina de Pascual", telefono: "666666666", curso: "2DAW" },
+           { nombre: "Marta de Pascual", telefono: "888888888", curso: "4Psicologia" },
+           { nombre: "Fernando Gomez", telefono: "777777777", curso: "2Aeronautica" }
+    ]
+});
+
