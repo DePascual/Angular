5b2d8ac803f2b5e23b4639bc1d5e3defa8e73e1a
DePascual <carolinadepascualjimenez@gmail.com>
2016-05-04 16:10:44 +0200

    Con formulario y ng-hide y ng-show

--- Angular2/Angular2/Scripts/misScripts.js
+++ Angular2/Angular2/Scripts/misScripts.js
@@ -5,4 +5,23 @@ cuaderno.controller('alumnosController', function ($scope) {
            { nombre: "Marta de Pascual", telefono: "888888888", curso: "4Psicologia" },
            { nombre: "Fernando Gomez", telefono: "777777777", curso: "2Aeronautica" }
     ]
+
+    
+
+    $scope.Save = function () {
+        $scope.alumnos.push({
+            nombre: $scope.nuevoAlumno.nombre,
+            telefono: $scope.nuevoAlumno.telefono,
+            curso: $scope.nuevoAlumno.curso,
+        })
+        $scope.formVisibility = false;
+        console.log($scope.formVisibility);
+    }
+    $scope.formVisibility = false;
+    $scope.ShowForm = function () {
+        $scope.formVisibility = true;
+        console.log($scope.formVisibility);
+    }
+
+
 });
